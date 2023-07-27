d1f86d53f91cb7105baf4378e55a3cd5e129b023
Martin Helmich <m.helmich@mittwald.de>
2023-07-27 16:18:47 +0200

    Fix class name

--- mw.rb
+++ mw.rb
@@ -1,4 +1,4 @@
-class MW < Formula
+class Mw < Formula
   desc "The mittwald command-line tool"
   homepage "https://github.com/mittwald/cli"
   url "https://mittwald-cli.s3.eu-central-1.amazonaws.com/versions/1.0.0-alpha.15/eb83bc7/mw-v1.0.0-alpha.15-eb83bc7-darwin-x64.tar.xz"
